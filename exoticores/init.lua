ef75f7873228de367e7868fb221f5cb4572e31a5
Tre <ThomasMonroe@protonmail.com>
2017-04-12 16:04:13 -0500

    Delete init.lua

--- exoticores/init.lua
+++
@@ -1,5 +0,0 @@
--- Load ore files--
-dofile(minetest.get_modpath("exoticores").."/nevadaite.lua")
-dofile(minetest.get_modpath("exoticores").."/cobaltominite.lua")
-dofile(minetest.get_modpath("exoticores").."/sideronatrite.lua")
-
