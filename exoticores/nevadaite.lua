eed916646e000f0fd309acbab1af8243a9062480
Tre <ThomasMonroe@protonmail.com>
2017-04-12 15:35:34 -0500

    Update nevadaite.lua

--- exoticores/nevadaite.lua
+++ exoticores/nevadaite.lua
@@ -86,3 +86,59 @@ minetest.register_ore({
 		y_min          = -31000,
 		y_max          = 0,
 	})
+
+--tools--
+
+minetest.register_tool("exoticores:pick_nevadaite", {
+	description = "Nevadaite Pickaxe",
+	inventory_image = "exoticores_tool_nevadaitepick.png",
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=3,
+		groupcaps={
+			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=60, maxlevel=3},
+		},
+		damage_groups = {fleshy=5},
+	},
+})
+
+minetest.register_tool("exoticores:shovel_nevadaite", {
+	description = "Nevadaite Shovel",
+	inventory_image = "exoticores_tool_nevadaiteshovel.png",
+	wield_image = "exoticores_tool_nevadaiteshovel.png^[transformR90",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		range = 5.0,
+		groupcaps={
+			crumbly = {times={[1]=1.10, [2]=0.50, [3]=0.30}, uses=60, maxlevel=3},
+		},
+		damage_groups = {fleshy=4},
+	},
+})
+
+minetest.register_tool("exoticores:axe_nevadaite", {
+	description = "Nevadaite Axe",
+	inventory_image = "exoticores_tool_nevadaiteaxe.png",
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=1,
+		groupcaps={
+			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=60, maxlevel=2},
+		},
+		damage_groups = {fleshy=7},
+	},
+})
+
+minetest.register_tool("exoticores:sword_nevadaite", {
+	description = "Nevadaite Sword",
+	inventory_image = "exoticores_tool_nevadaitesword.png",
+	tool_capabilities = {
+		full_punch_interval = 0.1,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=80, maxlevel=3},
+		},
+		damage_groups = {fleshy=8},
+	}
+})
