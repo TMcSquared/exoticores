25db30c5868736316cf29f7b07599f8a50f12241
ThomasMonroe <ThomasMonroe@protonmail.com>
2017-04-13 11:16:33 -0500

    added edoylerite to init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@
 dofile(minetest.get_modpath("exoticores").."/nevadaite.lua")
 dofile(minetest.get_modpath("exoticores").."/cobaltominite.lua")
 dofile(minetest.get_modpath("exoticores").."/sideronatrite.lua")
+dofile(minetest.get_modpath("exoticores").."/edoylerite.lua")
