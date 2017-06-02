b671a8dee3a488eb50226bd66b198d11196957e4
ThomasMonroe <ThomasMonroe@protonmail.com>
2017-06-02 14:09:30 -0500

    adjusted the physics_gravity

--- armor.lua
+++ armor.lua
@@ -4,31 +4,31 @@
 	minetest.register_tool("exoticores:helmet_nevadaite", {
 		description = "nevadaite Helmet",
 		inventory_image = "exoticores_armor_inv_helmet_nevadaite.png",
-		groups = {armor_head=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200},
+		groups = {armor_head=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200,physics_gravity=-.01},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:chestplate_nevadaite", {
 		description = "nevadaite Chestplate",
 		inventory_image = "exoticores_armor_inv_chestplate_nevadaite.png",
-		groups = {armor_torso=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200},
+		groups = {armor_torso=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200,physics_gravity=-.01},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:leggings_nevadaite", {
 		description = "nevadaite Leggings",
 		inventory_image = "exoticores_armor_inv_leggings_nevadaite.png",
-		groups = {armor_legs=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200},
+		groups = {armor_legs=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200,physics_gravity=-.01},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:boots_nevadaite", {
 		description = "nevadaite Boots",
 		inventory_image = "exoticores_armor_inv_boots_nevadaite.png",
-		groups = {armor_feet=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200},
+		groups = {armor_feet=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200,physics_gravity=-.01},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:shield_nevadaite", {
 		description = "nevadaite Shield",
 		inventory_image = "exoticores_armor_inv_shield_nevadaite.png",
-		groups = {armor_shield=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200},
+		groups = {armor_shield=15, armor_heal=10, physics_jump=0.1, armor_fire=1, armor_use=200,physics_gravity=-.01},
 		wear = 0,
 	})
 
@@ -36,31 +36,31 @@
 	minetest.register_tool("exoticores:helmet_cobaltominite", {
 		description = "cobaltominite Helmet",
 		inventory_image = "exoticores_armor_inv_helmet_cobaltominite.png",
-		groups = {armor_head=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100},
+		groups = {armor_head=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:chestplate_cobaltominite", {
 		description = "cobaltominite Chestplate",
 		inventory_image = "exoticores_armor_inv_chestplate_cobaltominite.png",
-		groups = {armor_torso=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100},
+		groups = {armor_torso=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:leggings_cobaltominite", {
 		description = "cobaltominite Leggings",
 		inventory_image = "exoticores_armor_inv_leggings_cobaltominite.png",
-		groups = {armor_legs=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100},
+		groups = {armor_legs=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:boots_cobaltominite", {
 		description = "cobaltominite Boots",
 		inventory_image = "exoticores_armor_inv_boots_cobaltominite.png",
-		groups = {armor_feet=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100},
+		groups = {armor_feet=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:shield_cobaltominite", {
 		description = "cobaltominite Shield",
 		inventory_image = "exoticores_armor_inv_shield_cobaltominite.png",
-		groups = {armor_shield=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100},
+		groups = {armor_shield=18, armor_heal=20, physics_jump=0.1, armor_fire=2, armor_use=100,physics_gravity=-.02},
 		wear = 0,
 	})
 
@@ -68,31 +68,31 @@
 	minetest.register_tool("exoticores:helmet_sideronatrite", {
 		description = "sideronatrite Helmet",
 		inventory_image = "exoticores_armor_inv_helmet_sideronatrite.png",
-		groups = {armor_head=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50},
+		groups = {armor_head=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:chestplate_sideronatrite", {
 		description = "sideronatrite Chestplate",
 		inventory_image = "exoticores_armor_inv_chestplate_sideronatrite.png",
-		groups = {armor_torso=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50},
+		groups = {armor_torso=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:leggings_sideronatrite", {
 		description = "sideronatrite Leggings",
 		inventory_image = "exoticores_armor_inv_leggings_sideronatrite.png",
-		groups = {armor_legs=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50},
+		groups = {armor_legs=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:boots_sideronatrite", {
 		description = "sideronatrite Boots",
 		inventory_image = "exoticores_armor_inv_boots_sideronatrite.png",
-		groups = {armor_feet=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50},
+		groups = {armor_feet=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:shield_sideronatrite", {
 		description = "sideronatrite Shield",
 		inventory_image = "exoticores_armor_inv_shield_sideronatrite.png",
-		groups = {armor_shield=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50},
+		groups = {armor_shield=18, armor_heal=30, physics_jump=0.1, armor_fire=4, armor_use=50,physics_gravity=-.04},
 		wear = 0,
 	})
 
@@ -100,31 +100,31 @@
 	minetest.register_tool("exoticores:helmet_edoylerite", {
 		description = "edoylerite Helmet",
 		inventory_image = "exoticores_armor_inv_helmet_edoylerite.png",
-		groups = {armor_head=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25},
+		groups = {armor_head=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:chestplate_edoylerite", {
 		description = "edoylerite Chestplate",
 		inventory_image = "exoticores_armor_inv_chestplate_edoylerite.png",
-		groups = {armor_torso=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25},
+		groups = {armor_torso=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.16},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:leggings_edoylerite", {
 		description = "edoylerite Leggings",
 		inventory_image = "exoticores_armor_inv_leggings_edoylerite.png",
-		groups = {armor_legs=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25},
+		groups = {armor_legs=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:boots_edoylerite", {
 		description = "edoylerite Boots",
 		inventory_image = "exoticores_armor_inv_boots_edoylerite.png",
-		groups = {armor_feet=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25},
+		groups = {armor_feet=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:shield_edoylerite", {
 		description = "edoylerite Shield",
 		inventory_image = "exoticores_armor_inv_shield_edoylerite.png",
-		groups = {armor_shield=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25},
+		groups = {armor_shield=18, armor_heal=40, physics_jump=0.1, armor_fire=6, armor_use=25,physics_gravity=-.08},
 		wear = 0,
 	})
 
@@ -132,31 +132,31 @@
 	minetest.register_tool("exoticores:helmet_lonsdaleite", {
 		description = "lonsdaleite Helmet",
 		inventory_image = "exoticores_armor_inv_helmet_lonsdaleite.png",
-		groups = {armor_head=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10},
+		groups = {armor_head=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10,physics_gravity=-.16},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:chestplate_lonsdaleite", {
 		description = "lonsdaleite Chestplate",
 		inventory_image = "exoticores_armor_inv_chestplate_lonsdaleite.png",
-		groups = {armor_torso=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10},
+		groups = {armor_torso=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10,physics_gravity=-.16},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:leggings_lonsdaleite", {
 		description = "lonsdaleite Leggings",
 		inventory_image = "exoticores_armor_inv_leggings_lonsdaleite.png",
-		groups = {armor_legs=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10},
+		groups = {armor_legs=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10,physics_gravity=-.16},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:boots_lonsdaleite", {
 		description = "lonsdaleite Boots",
 		inventory_image = "exoticores_armor_inv_boots_lonsdaleite.png",
-		groups = {armor_feet=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10},
+		groups = {armor_feet=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10,physics_gravity=-.16},
 		wear = 0,
 	})
 	minetest.register_tool("exoticores:shield_lonsdaleite", {
 		description = "lonsdaleite Shield",
 		inventory_image = "exoticores_armor_inv_shield_lonsdaleite.png",
-		groups = {armor_shield=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10},
+		groups = {armor_shield=18, armor_heal=100, physics_jump=0.1, armor_fire=10, armor_use=10,physics_gravity=-.16},
 		wear = 0,
 	})
 
